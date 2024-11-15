50051d99e7829eb206966db50e36d57570686655
she2024 <bullybluedog@hotmail.com>
2024-11-16 09:55:19 +1000

    feat: Completed js classes

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -1,16 +1,48 @@
-class Dog{
-    constructor (name, breed, age, colour){
+class Dog {
+    constructor(name, breed, age, color){
         this.name = name;
         this.breed = breed;
         this.age = age;
-        this.colour = colour;
+        this.color = color;
+        Dog.#numberOfDogs++;
+    }
+    static #numberOfDogs = 0;
+
+    static resetDogCount(){
+        Dog.#numberOfDogs = 0;
     }
 
-    howl(){
+    // static set numberOfDogs(newValue){
+    //     return Dog.#numberOfDogs++;
+    // }
 
+    bark() {
+        // console.log(`Woof! Woof! I am ${this.name}.`);
+        throw new BarkError(this.name, this.breed);
     }
 }
 
-let brockie = new Dog ('Brockie', 'Germany Shepherd', '3', 'Brown');
+let brockie = new Dog('Brockie', 'German Shepherd', '3', 'Brown');
 
-console.log(brockie);
+// console.log(brockie.bark());
+
+// create unique BarkError class by inheriting from Error class
+class BarkError extends Error {
+    constructor(dogName, dogBreed){
+        // super() is called to use the Error class's properties/methods
+        super(`${dogName} of breed ${dogBreed} failed to bark.`);
+    }
+}
+
+// let newError = new Error()
+try {
+    brockie.bark();
+} catch (error) {
+    // throw new BarkError(brockie.name, brockie.breed);
+    if (BarkError){
+        console.log("Bark error occured.");
+    }
+}
+
+Dog.numberOfDogs = 10;
+console.log(Dog.numberOfDogs);
