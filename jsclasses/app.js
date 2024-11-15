60a7da3e760a6c498b1eac9fa1c1fe43f16ef39a
she2024 <bullybluedog@hotmail.com>
2024-11-16 09:02:59 +1000

    feat: Example Dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,16 @@
+class Dog{
+    constructor (name, breed, age, colour){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    howl(){
+
+    }
+}
+
+let brockie = new Dog ('Brockie', 'Germany Shepherd', '3', 'Brown');
+
+console.log(brockie);
